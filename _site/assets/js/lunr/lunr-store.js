c196a5410266558092e531abbbd2e3a6df619376
Mike Medley <mike.medley@digitalreasoning.com>
2019-05-22 13:22:44 -0500

    Content update

--- _site/assets/js/lunr/lunr-store.js
+++ _site/assets/js/lunr/lunr-store.js
@@ -1,1 +1,6 @@
-var store = []
+var store = [{
+        "title": "Hello World",
+        "excerpt":"#Hello World   This is my first line.  ","categories": [],
+        "tags": [],
+        "url": "http://localhost:4000/Hello-world/",
+        "teaser":null}]
