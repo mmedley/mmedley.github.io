747a35281b1736181a5f5648230eb33bb26f44d3
mmedley <eproms73@gmail.com>
2025-01-20 18:51:49 -0600

    major update
    Signed-off-by: mmedley <eproms73@gmail.com>

--- __archive/README.md
+++
@@ -1,7 +0,0 @@
-# mmedley.github.io
-
-# install asdf
-# setup ruby on asdf - https://mac.install.guide/rubyonrails/7
-# asdf install ruby latest
-# asdf global ruby 3.4.1
-# https://mac.install.guide/rubyonrails/7
--- __archive/_site/assets/js/lunr/lunr-store.js
+++
@@ -1,6 +0,0 @@
-var store = [{
-        "title": "Hello World",
-        "excerpt":"#Hello World   This is my first line.  ","categories": [],
-        "tags": [],
-        "url": "http://localhost:4000/Hello-world/",
-        "teaser":null}]
